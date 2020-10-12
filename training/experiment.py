0c23762c86948c2f94a93af696aaf82a54a544c0
Cristian Garcia <cgarcia.e88@gmail.com>
2020-10-11 19:35:44 -0500

    save

---
+++ training/experiment.py
@@ -0,0 +1,55 @@
+import elegy
+import jax
+import numpy as np
+import typer
+
+from .made import LinearMADE
+
+
+def main(debug: bool = False):
+
+    if debug:
+        import debugpy
+
+        print("Waiting debugger...")
+        debugpy.listen(5678)
+        debugpy.wait_for_client()
+
+    x = np.random.uniform(size=(10, 3))
+
+    def f(x):
+        linear_m1 = LinearMADE(5, n_features=3)
+        linear_m2 = LinearMADE(5, n_features=3)
+        linear_m3 = LinearMADE(3, n_features=3, is_output=True)
+
+        assignments = np.arange(3)
+        x, assignments = linear_m1(x, assignments)
+        x, assignments = linear_m2(x, assignments)
+        x, assignments = linear_m3(x, assignments)
+
+        return x
+
+    grads = jax.grad(lambda x: f(x)[0, 2])(x)[0]
+
+    print(grads)
+
+
+class MADE(elegy.Module):
+    def __init__(self, n_features: int, **kwargs):
+        super().__init__(**kwargs)
+        self.n_features = n_features
+
+    def call(self, x):
+        assert self.n_features == x.shape[-1]
+
+        assignments = np.arange(self.n_features)
+
+        x, assignments = LinearMADE(32, n_features=self.n_features)(x, assignments)
+        x = jax.nn.elu(x)
+
+        x, assignments = LinearMADE(32, n_features=self.n_features)(x, assignments)
+        x = jax.nn.elu(x)
+
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ training/made.py
@@ -0,0 +1,123 @@
+import typing as tp
+
+import elegy
+import haiku as hk
+import jax.numpy as jnp
+import numpy as np
+from elegy import module
+from elegy.initializers import TruncatedNormal
+from elegy.types import Initializer
+
+
+class MaskInfo(tp.NamedTuple):
+    mask: np.ndarray
+    assignments: np.ndarray
+
+
+class LinearMADE(module.Module):
+    """Linear module."""
+
+    w: np.ndarray
+    b: np.ndarray
+    mask_info: np.ndarray
+
+    def __init__(
+        self,
+        output_size: int,
+        n_features: int,
+        is_output: bool = False,
+        with_bias: bool = True,
+        w_init: tp.Optional[Initializer] = None,
+        b_init: tp.Optional[Initializer] = None,
+        **kwargs
+    ):
+        """
+        Constructs the Linear module.
+
+        Arguments:
+            output_size: Output dimensionality.
+            with_bias: Whether to add a bias to the output.
+            w_init: Optional initializer for weights. By default, uses random values
+                from truncated normal, with stddev `1 / sqrt(fan_in)`. See
+                https://arxiv.org/abs/1502.03167v3.
+            b_init: Optional initializer for bias. By default, zero.
+            kwargs: Additional keyword arguments passed to Module.
+        """
+        super().__init__(**kwargs)
+        self.input_size = None
+        self.output_size = output_size
+        self.n_features = n_features
+        self.is_output = is_output
+        self.with_bias = with_bias
+        self.w_init = w_init
+        self.b_init = b_init or jnp.zeros
+
+    @staticmethod
+    def create_mask(
+        output_size: int,
+        n_features: int,
+        input_assignments: np.ndarray,
+        is_output: bool = False,
+    ):
+        if not is_output:
+            n_features -= 1
+
+        input_size = input_assignments.shape[0]
+
+        output_assignments = list(range(n_features)) * int(output_size / n_features + 1)
+        output_assignments = np.array(output_assignments[:output_size])
+
+        kernel_shape = (input_size, output_size)
+
+        input_constraints = np.broadcast_to(input_assignments[:, None], kernel_shape)
+        output_constraints = np.broadcast_to(output_assignments[None, :], kernel_shape)
+
+        if is_output:
+            kernel_mask = (output_constraints > input_constraints).astype(np.int32)
+        else:
+            kernel_mask = (output_constraints >= input_constraints).astype(np.int32)
+
+        return MaskInfo(kernel_mask, output_assignments)
+
+    def call(self, inputs: np.ndarray, input_assignments: np.ndarray) -> np.ndarray:
+        """"""
+        if not inputs.shape:
+            raise ValueError("Input must not be scalar.")
+
+        input_size = self.input_size = inputs.shape[-1]
+        output_size = self.output_size
+        dtype = inputs.dtype
+
+        w_init = self.w_init
+
+        if w_init is None:
+            stddev = 1.0 / np.sqrt(self.input_size)
+            w_init = TruncatedNormal(stddev=stddev)
+
+        w = self.add_parameter(
+            "w", [input_size, output_size], dtype, initializer=w_init
+        )
+
+        w_mask, output_assignments = self.add_parameter(
+            "mask_info",
+            initializer=lambda *args: self.create_mask(
+                output_size=output_size,
+                n_features=self.n_features,
+                input_assignments=input_assignments,
+                is_output=self.is_output,
+            ),
+            trainable=False,
+        )
+
+        w *= w_mask
+
+        out = jnp.dot(inputs, w)
+
+        if self.with_bias:
+            b = self.add_parameter(
+                "b", [self.output_size], dtype, initializer=self.b_init
+            )
+            b = jnp.broadcast_to(b, out.shape)
+            out = out + b
+
+        return out, output_assignments
